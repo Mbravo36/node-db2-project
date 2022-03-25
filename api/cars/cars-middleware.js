ebb4e25c584c605728ce57d0cda43d275adadd34
Your Name <mari@Maribels-MacBook-Pro.local>
2022-03-25 16:27:30 -0500

    solved

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,60 @@
+const Cars = require('./cars-model')
+const vin = require('vin-validator')
+
+
 const checkCarId = (req, res, next) => {
   // DO YOUR MAGIC
+  const car =  Cars.getById(req.params.id)
+    if(!car){
+      next({status: 404, message: 'not found'})
+    }else{
+      req.car = car
+      next()
+    }
 }
 
 const checkCarPayload = (req, res, next) => {
   // DO YOUR MAGIC
+  if (!req.body.vin) return next({
+    status: 400,
+    message: `vin is missing`
+  })
+  if (!req.body.make) return next({
+    status: 400,
+    message: `make is missing`
+  })
+  if (!req.body.model) return next({
+    status: 400,
+    message: `model is missing`
+  })
+  if (!req.body.mileage) return next({
+    status: 400,
+    message: `mileage is missing`
+  })
+  next()
 }
 
 const checkVinNumberValid = (req, res, next) => {
   // DO YOUR MAGIC
+  if (vin.validate(req.body.vin)){
+    next()
+  }else{
+    next({ status: 400, message: `vin ${req.body.vin} is invalid`})
+  }
 }
 
 const checkVinNumberUnique = (req, res, next) => {
   // DO YOUR MAGIC
+  const existing =  Cars.getByVin(req.body.vin)
+  if(!existing){
+    next()
+  }else{
+    next({status: 400, message:`vin ${req.body.vin} already exists`})
+  }
 }
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberUnique,
+  checkVinNumberValid
+} 
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,41 @@
 // DO YOUR MAGIC
+const Cars = require('./cars-model')
+const {
+    checkCarId,
+    checkCarPayload,
+    checkVinNumberUnique,
+    checkVinNumberValid
+} = require('./cars-middleware')
+
+const router = require('express').Router()
+
+
+router.get('/', async (req, res, next) => {
+    try {
+        const cars = await Cars.getAll()
+        res.json(cars)
+    } catch (error) {
+        next(error)
+    }
+
+
+})
+
+router.get('/:id', checkCarId, (req, res, next) => {
+    res.json(req.car)
+})
+
+router.post('/', 
+checkCarPayload, 
+checkVinNumberValid,
+checkVinNumberUnique, 
+async(req, res, next) => {
+    try {
+        const car = await Cars.create(req.body)
+        res.json(car)
+    } catch (error) {
+        next(error)
+    }
+})
+
+module.exports = router 
